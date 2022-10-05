361626ba154b8656405096b88ff119517a2072a7
Francisco Carranza <Carranza12>
2022-10-05 12:05:52 -0500

    correccion del menu de siguiente y anterior

--- js/form.js
+++ js/form.js
@@ -4,65 +4,59 @@ let currentStep="stepInitial";
 let stepNext="stepOne"
 let currentCircle="circleInitial"
 let CircleNext="circleOne"
-
+let stepAnterior='stepInitial'
+let circleAnterior='circleInitial'
 
 const btnPrevStep=window.document.getElementById("btnPrevStep");
 
 
-const nextStep=(stepActual,stepSiguiente,circleActual,circleSiguiente)=>{
-    console.log('step actual:',stepActual,' step siguiente:',stepNext)
-    document.getElementById(circleActual).classList.remove('active')
-    document.getElementById(circleSiguiente).classList.add('active')
-    document.getElementById(stepActual).style.display="none";
-    document.getElementById(stepSiguiente).style.display="block";
-    currentCircle=circleSiguiente
-    circleActual=currentCircle
-    currentStep=stepSiguiente
-    stepActual=currentStep
-    if(currentCircle==='circleInitial') CircleNext='circleOne'
-    if(currentCircle==='circleOne') CircleNext='circleTwo'
-    if(currentCircle==='circleTwo') CircleNext='circleThree'
-    if(currentCircle==='circleThree') CircleNext='circleFour'
-    if(currentCircle==='circleFour') CircleNext='circleFive'
-    if(currentStep==='stepInitial') stepNext='stepOne'
-    if(currentStep==='stepOne') stepNext='stepTwo'
-    if(currentStep==='stepTwo') stepNext='stepThree'
-    if(currentStep==='stepThree') stepNext='stepFour'
-    if(currentStep==='stepFour') stepNext='stepFive'
-    if(currentStep==='stepFive')stepNext='stepEnd'
-
-    if(currentStep==='stepEnd'){
-        stepNext=''
-        circlesContainer.style.display="none"
-        btnNextStep.innerText="Finalizar"
+const nextStep=(paso_actual)=>{
+    if(paso_actual==='stepInitial') {
+        document.getElementById('stepInitial').style.display="none";
+        document.getElementById('stepOne').style.display="block";
     }
+    if(paso_actual==='stepOne'){
+        document.getElementById('stepOne').style.display="none";
+        document.getElementById('stepTwo').style.display="block";
+    }
+
+    if(paso_actual==='stepTwo'){
+        document.getElementById('stepTwo').style.display="none";
+        document.getElementById('stepThree').style.display="block";
+    }
+    if(paso_actual==='stepThree'){
+        document.getElementById('stepThree').style.display="none";
+        document.getElementById('stepFour').style.display="block";
+    }
+    if(paso_actual==='stepFour'){
+        document.getElementById('stepFour').style.display="none";
+        document.getElementById('stepFive').style.display="block";
+    }
+   
 }
 
 
-const prevStep=(stepActual,stepSiguiente,circleActual,circleSiguiente)=>{
-    console.log('step actual:',stepActual,' step siguiente:',stepNext)
-    document.getElementById(circleActual).classList.remove('active')
-    document.getElementById(circleSiguiente).classList.add('active')
-    document.getElementById(stepActual).style.display="none";
-    document.getElementById(stepSiguiente).style.display="block";
-    currentCircle=circleSiguiente
-    circleActual=currentCircle
-    currentStep=stepSiguiente
-    stepActual=currentStep
-    if(currentCircle==='circleOne') CircleNext='circleInitial'
-    if(currentCircle==='circleTwo') CircleNext='circleOne'
-    if(currentCircle==='circleThree') CircleNext='circleTwo'
-    if(currentCircle==='circleFour') CircleNext='circleThree'
-    if(currentCircle==='circleFive') CircleNext='circleFour'
+const prevStep=(paso_actual)=>{  
+    if(paso_actual==='stepOne'){
+        document.getElementById('stepOne').style.display="none";
+        document.getElementById('stepInitial').style.display="block";
+    }
 
-    if(currentStep==='stepOne') stepNext='stepInitial'
-    if(currentStep==='stepTwo') stepNext='stepOne'
-    if(currentStep==='stepThree') stepNext='stepTwo'
-    if(currentStep==='stepFour') stepNext='stepThree'
-    if(currentStep==='stepFive')stepNext='stepFour'
-
-    if(currentStep==='stepEnd'){
-        stepNext='stepFive'
+    if(paso_actual==='stepTwo'){
+        document.getElementById('stepTwo').style.display="none";
+        document.getElementById('stepOne').style.display="block";
+    }
+    if(paso_actual==='stepThree'){
+        document.getElementById('stepThree').style.display="none";
+        document.getElementById('stepTwo').style.display="block";
+    }
+    if(paso_actual==='stepFour'){
+        document.getElementById('stepFour').style.display="none";
+        document.getElementById('stepThree').style.display="block";
+    }
+    if(paso_actual==='stepFive'){
+        document.getElementById('stepFive').style.display="none";
+        document.getElementById('stepFour').style.display="block";
     }
 }
 
