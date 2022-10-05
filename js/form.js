b4a5707767914db3d879788d2e826c02ee326e5c
streakycoast <christianchiquito6@gmail.com>
2022-10-04 19:36:16 -0500

    agregando brn regresar no completado

--- js/form.js
+++ js/form.js
@@ -5,6 +5,10 @@ let stepNext="stepOne"
 let currentCircle="circleInitial"
 let CircleNext="circleOne"
 
+
+const btnPrevStep=window.document.getElementById("btnPrevStep");
+
+
 const nextStep=(stepActual,stepSiguiente,circleActual,circleSiguiente)=>{
     console.log('step actual:',stepActual,' step siguiente:',stepNext)
     document.getElementById(circleActual).classList.remove('active')
@@ -31,6 +35,34 @@ const nextStep=(stepActual,stepSiguiente,circleActual,circleSiguiente)=>{
         stepNext=''
         circlesContainer.style.display="none"
         btnNextStep.innerText="Finalizar"
+    }
+}
+
+
+const prevStep=(stepActual,stepSiguiente,circleActual,circleSiguiente)=>{
+    console.log('step actual:',stepActual,' step siguiente:',stepNext)
+    document.getElementById(circleActual).classList.remove('active')
+    document.getElementById(circleSiguiente).classList.add('active')
+    document.getElementById(stepActual).style.display="none";
+    document.getElementById(stepSiguiente).style.display="block";
+    currentCircle=circleSiguiente
+    circleActual=currentCircle
+    currentStep=stepSiguiente
+    stepActual=currentStep
+    if(currentCircle==='circleOne') CircleNext='circleInitial'
+    if(currentCircle==='circleTwo') CircleNext='circleOne'
+    if(currentCircle==='circleThree') CircleNext='circleTwo'
+    if(currentCircle==='circleFour') CircleNext='circleThree'
+    if(currentCircle==='circleFive') CircleNext='circleFour'
+
+    if(currentStep==='stepOne') stepNext='stepInitial'
+    if(currentStep==='stepTwo') stepNext='stepOne'
+    if(currentStep==='stepThree') stepNext='stepTwo'
+    if(currentStep==='stepFour') stepNext='stepThree'
+    if(currentStep==='stepFive')stepNext='stepFour'
+
+    if(currentStep==='stepEnd'){
+        stepNext='stepFive'
     }
 }
 
