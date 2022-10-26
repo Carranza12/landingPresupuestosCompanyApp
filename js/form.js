987c3a665e00d66afe9e3240314a3643971f3641
Francisco Carranza <Carranza12>
2022-10-25 22:44:51 -0500

    cambios

--- js/form.js
+++ js/form.js
@@ -8,6 +8,28 @@ let stepAnterior='stepInitial'
 let circleAnterior='circleInitial'
 
 const btnPrevStep=window.document.getElementById("btnPrevStep");
+let type_job="";
+window.document.querySelector('#type_job').addEventListener('change',()=>{
+   type_job= document.querySelector('#type_job').value;
+   console.log('change')
+})
+
+const saveData=()=>{
+    const nombre = window.document.getElementById("txtNombre").vallue;
+    const email = window.document.getElementById("txtEnail").value;
+    const phone = window.document.getElementById("txtTelefono").value;
+   
+ 
+   
+    const document={
+        name,
+        email,
+        phone,
+        type_job
+    }
+    console.log(document);
+}
+
 
 
 const nextStep=(paso_actual)=>{
@@ -23,6 +45,7 @@ const nextStep=(paso_actual)=>{
     if(paso_actual==='stepTwo'){
         document.getElementById('stepTwo').style.display="none";
         document.getElementById('stepThree').style.display="block";
+        saveData();
     }
     if(paso_actual==='stepThree'){
         document.getElementById('stepThree').style.display="none";
