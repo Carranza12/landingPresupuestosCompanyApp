d5a1b1496d339d079156e87ed41d76cac35c24c9
streakycoast <christianchiquito6@gmail.com>
2022-10-29 16:33:00 -0500

    Iniciando validaciones

--- js/form.js
+++ js/form.js
@@ -64,3 +64,42 @@ const prevStep=(paso_actual)=>{
     }
 }
 
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+
+const validarFormulario = (e)=>{
+    switch(e.target.name){
+        case "nombre":
+        if(expresiones.nombre.test(e.target.value)){
+            document.getElementById('grupo_nombre').classList.remove('formulario__grupo-incorrecto');
+            document.getElementById('grupo_nombre').classList.add('formulario__grupo-correcto');
+        }
+        else{
+            document.getElementById('grupo_nombre').classList.add('formulario__grupo-incorrecto');
+        }
+            break;
+        case "email":
+
+            break;
+        case "telefono":
+
+            break;
+    }
+   }
+   
+
+
+inputs.forEach((input)=>{
+input.addEventListener('keyup', validarFormulario);
+input.addEventListener('blur', validarFormulario);
+})
+formulario.addEventListener('submit', (e) =>{
+e.preventDefault();
+})
+
